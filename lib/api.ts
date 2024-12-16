a7df70e121b0570a554794d3d9738be435d7fd50
asaldanam <asaldanam@gmail.com>
2024-12-17 00:05:48 +0100

    fix: env

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,8 @@
+const { NEXT_PUBLIC_BASE_URL = 'http://localhost:8080' } = process.env;
+
 export const api = {
     get: async <Data>(input: RequestInfo | URL, init?: Omit<RequestInit, 'method'>): Promise<Data> => {
-        const response = await fetch(`${process.env.BASE_URL}/${input}`, { ...init, method: 'GET', cache: 'no-cache' });
+        const response = await fetch(`${NEXT_PUBLIC_BASE_URL}/${input}`, { ...init, method: 'GET', cache: 'no-cache' });
         const data: Data = await response.json();
 
         return data;
